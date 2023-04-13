46866c91e8b5c1a3ff4e88feeed2f6fc895e55dc
arskov26 <130170740+arskov26@users.noreply.github.com>
2023-04-13 19:14:30 +0300

    Add files via upload

---
+++ Project#3.sql
@@ -0,0 +1,98 @@
+USE Project
+SELECT Saledate, CONVERT(Date,Saledate)
+FROM NashvilleHousing
+
+UPDATE NashvilleHousing
+SET SaleDate = CONVERT(Date,Saledate)
+
+ALTER TABLE NashvilleHousing
+ADD SaleDateConverted Date;
+--Конвертируем формат столбца SaleDate
+UPDATE NashvilleHousing
+SET SaleDateConverted = CONVERT(Date,SaleDate)
+
+SELECT PropertyAddress 
+FROM NashvilleHousing;
+--Заполним столбец Address Data
+SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress,ISNULL(a.PropertyAddress,b.PropertyAddress)
+FROM NashvilleHousing a
+JOIN NashvilleHousing b
+ON a.ParcelID = b.ParcelID
+AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL;
+
+UPDATE a
+SET PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
+FROM NashvilleHousing a
+JOIN NashvilleHousing b
+ON a.ParcelID = b.ParcelID
+AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress IS NULL;
+--Разобьем столбец Адрес на 3 столбца (Адрес, город, населенный пункт)
+SELECT SUBSTRING(
+PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1) AS Address,
+SUBSTRING(
+PropertyAddress, CHARINDEX(',', PropertyAddress)+1, LEN(PropertyAddress)) AS Address
+FROM dbo.NashvilleHousing
+ALTER TABLE NashvilleHousing
+ADD PropertySplitCity Nvarchar(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1, LEN(PropertyAddress))
+SELECT * FROM NashvilleHousing
+
+SELECT
+PARSENAME(REPLACE(OwnerAddress,',','.'),3),
+PARSENAME(REPLACE(OwnerAddress,',','.'),2),
+PARSENAME(REPLACE(OwnerAddress,',','.'),1)
+FROM NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitAddress Nvarchar(255);
+UPDATE NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress,',','.'),3);
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitCity Nvarchar(255);
+UPDATE NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress,',','.'),2);
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitState Nvarchar(255);
+UPDATE NashvilleHousing
+SET OwnerSplitState =  PARSENAME(REPLACE(OwnerAddress,',','.'),1);
+
+--Заменим 'Y' и 'N' на 'YES' и 'NO' в столбце SoldAsVacant
+SELECT Distinct(SoldAsVacant), COUNT(SoldAsVacant)
+FROM NashvilleHousing
+GROUP BY SoldAsVacant
+ORDER BY 2
+
+SELECT SoldAsVacant,
+CASE WHEN SoldAsVacant = 'Y' THEN 'YES'
+	 WHEN SoldAsVacant = 'N' THEN 'NO'
+	 ELSE SoldAsVacant
+	 END
+	 FROM NashvilleHousing
+--Удаляем дубликаты
+WITH RownumCTE AS(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY
+ParcelID,
+PropertyAddress,
+SalePrice,
+SaleDate,
+LegalReference
+ORDER BY UniqueID) row_num
+FROM NashvilleHousing
+)
+SELECT * FROM RownumCTE
+WHERE row_num > 1
+ORDER BY PropertyAddress
+--Удаляем неиспользующиеся колонки
+SELECT * FROM NashvilleHousing
+ALTER TABLE NashvilleHousing
+DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress
+ALTER TABLE NashvilleHousing
+DROP COLUMN SaleDate
